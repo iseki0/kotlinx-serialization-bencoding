bca918caed81da2bdba8e6f6750bbde28e02bc2e
iseki <admin@iseki.space>
2023-03-31 21:52:15 +0800

    ci: add github action

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,26 +18,75 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+tasks.withType<AbstractArchiveTask>().configureEach {
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
 }
 
 kotlin {
     jvmToolchain(17)
 }
 
-publishing{
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/iseki/kotlinx-serialization-bencode")
+            url = uri("https://maven.pkg.github.com/iseki0/kotlinx-serialization-bencode")
             credentials {
                 username = project.findProperty("gpr.user") as? String ?: System.getenv("USERNAME")
                 password = project.findProperty("gpr.key") as? String ?: System.getenv("TOKEN")
             }
         }
+        maven {
+            name = "Central"
+            url = if (version.toString().endsWith("SNAPSHOT")) {
+                // uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+                uri("https://oss.sonatype.org/content/repositories/snapshots")
+            } else {
+                // uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
+                uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+            }
+            credentials {
+                username = properties["ossrhUsername"]?.toString() ?: System.getenv("OSSRH_USERNAME")
+                password = properties["ossrhPassword"]?.toString() ?: System.getenv("OSSRH_PASSWORD")
+            }
+        }
     }
     publications {
-        register<MavenPublication>("gpr"){
+        register<MavenPublication>("gpr") {
             from(components["java"])
+            pom {
+                description.set("Bencode codec of kotlin serialization")
+                url.set("https://github.com/iseki0/kotlinx-serialization-bencode")
+                licenses {
+                    license {
+                        name.set("Apache-2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("iseki0")
+                        name.set("iseki zero")
+                        email.set("iseki@iseki.space")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/iseki0/kotlinx-serialization-bencode.git")
+                    developerConnection.set("scm:git:ssh://github.com/iseki0/kotlinx-serialization-bencode.git")
+                    url.set("https://github.com/iseki0/kotlinx-serialization-bencode")
+                }
+            }
         }
     }
 }
