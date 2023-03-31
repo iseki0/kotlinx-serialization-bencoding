a77710d9640923f6a0bc47ddc21dee0ca3ec70c4
iseki <admin@iseki.space>
2023-03-31 21:49:10 +0800

    build: prepare for  github publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     kotlin("jvm") version "1.8.10"
     kotlin("plugin.serialization") version "1.8.10"
+    `maven-publish`
 }
 
 group = "space.iseki.bencode"
-version = "1.0-SNAPSHOT"
+version = "0.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -21,4 +22,22 @@ tasks.test {
 
 kotlin {
     jvmToolchain(17)
+}
+
+publishing{
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/iseki/kotlinx-serialization-bencode")
+            credentials {
+                username = project.findProperty("gpr.user") as? String ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as? String ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr"){
+            from(components["java"])
+        }
+    }
 }
