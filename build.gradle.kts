eb31a150c9cdbbf247e2623caaeefa0bcd74ea81
iseki <admin@iseki.space>
2023-05-29 20:47:35 +0800

    refactor: WIP

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,8 @@ allprojects {
     group = "space.iseki.bencoding"
     if (version == "unspecified") version = "0.1.0-SNAPSHOT"
 }
+
+dependencies {
+    commonMainApi(libs.kotlinx.serialization.json)
+    commonMainCompileOnly(libs.okio)
+}
---
+++ src/commonMain/kotlin/space/iseki/bencoding/BencodingDecoder.kt
@@ -0,0 +1,5 @@
+package space.iseki.bencoding
+
+interface Input
+
+
