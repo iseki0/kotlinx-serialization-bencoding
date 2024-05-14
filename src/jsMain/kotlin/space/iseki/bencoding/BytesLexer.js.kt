51647947bb3a1e8903431d39ea64b6891867f2ec
iseki <admin@iseki.space>
2024-05-15 03:32:57 +0800

    fix: shared util bytes2StringIso88591 in JS target should ensure return value type

--- src/jsMain/kotlin/space/iseki/bencoding/BytesLexer.js.kt
+++ src/jsMain/kotlin/space/iseki/bencoding/BytesLexer.js.kt
@@ -4,7 +4,7 @@ internal actual fun bytes2Long(bytes: ByteArray, off: Int, len: Int): Long =
     bytes.copyOfRange(off, off + len).decodeToString().toLong()
 
 internal actual fun bytes2StringIso88591(bytes: ByteArray, off: Int, len: Int): String {
-    return js("new TextDecoder('windows-1252').decode(bytes.slice(off, off + len))")
+    return js("new TextDecoder('windows-1252').decode(bytes.slice(off, off + len))") as String
 }
 
 internal actual fun string2BytesIso88591(s: String): ByteArray {
