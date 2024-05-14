8ffd4261328270104ee3f63b21c7b39395c47341
iseki <admin@iseki.space>
2024-05-15 03:34:23 +0800

    refactor: add helper interface BWriter

---
+++ src/commonMain/kotlin/space/iseki/bencoding/BWriter.kt
@@ -0,0 +1,29 @@
+package space.iseki.bencoding
+
+internal interface BWriter {
+    fun writeByte(b: Int)
+    fun writeBytes(b: ByteArray)
+    fun writeEnd() {
+        writeByte('e'.code)
+    }
+
+    fun writeDictBegin() {
+        writeByte('d'.code)
+    }
+
+    fun writeListBegin() {
+        writeByte('l'.code)
+    }
+
+    fun writeInt(i: Long) {
+        writeByte('i'.code)
+        writeBytes(i.toString().encodeToByteArray())
+        writeEnd()
+    }
+
+    fun writeString(s: String) {
+        writeBytes(s.length.toString().encodeToByteArray())
+        writeByte(':'.code)
+        writeBytes(s.encodeToByteArray())
+    }
+}
