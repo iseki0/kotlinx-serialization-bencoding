34cbfaf3f719b4cc4448f4a47601959a0f91e65a
iseki <admin@iseki.space>
2024-05-15 23:51:11 +0800

    refactor(jvm): Call StandardCharsets instead of Charsets

--- src/jvmMain/kotlin/space/iseki/bencoding/Utils.kt
+++ src/jvmMain/kotlin/space/iseki/bencoding/Utils.kt
@@ -1,7 +1,9 @@
 package space.iseki.bencoding
 
+import java.nio.charset.StandardCharsets
+
 internal actual fun bytes2Long(bytes: ByteArray, off: Int, len: Int): Long = String(bytes, off, len).toLong()
 internal actual fun bytes2StringIso88591(bytes: ByteArray, off: Int, len: Int): String =
-    String(bytes, off, len, Charsets.ISO_8859_1)
+    String(bytes, off, len, StandardCharsets.ISO_8859_1)
 
-internal actual fun string2BytesIso88591(s: String): ByteArray = s.toByteArray(Charsets.ISO_8859_1)
+internal actual fun string2BytesIso88591(s: String): ByteArray = s.toByteArray(StandardCharsets.ISO_8859_1)
