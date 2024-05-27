f5947b4bd5498da190c94e0a489bddb229272eeb
iseki <admin@iseki.space>
2024-05-27 23:43:34 +0800

    style: remove unused import

--- src/commonMain/kotlin/space/iseki/bencoding/internal/Utils.kt
+++ src/commonMain/kotlin/space/iseki/bencoding/internal/Utils.kt
@@ -3,7 +3,6 @@ package space.iseki.bencoding.internal
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.descriptors.SerialDescriptor
 import space.iseki.bencoding.BinaryString
-import kotlin.math.max
 
 internal expect fun bytes2Long(bytes: ByteArray, off: Int, len: Int): Long
 internal expect fun bytes2StringIso88591(bytes: ByteArray, off: Int = 0, len: Int = bytes.size): String?
