06199ba161f571322e6126e946a6d968e1fe9ab0
iseki <admin@iseki.space>
2025-01-07 09:18:19 +0800

    build: remove buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
 import org.gradle.jvm.tasks.Jar
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.util.*
 
 plugins {
-    kotlin("multiplatform")
-    kotlin("plugin.serialization")
-    id("org.jetbrains.kotlinx.kover")
-    id("org.jetbrains.kotlinx.binary-compatibility-validator")
-    `maven-publish-convention`
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlinx.kover)
+    alias(libs.plugins.kotlinx.binary.compatibility.validator)
+    `maven-publish`
+    signing
 }
 
 allprojects {
@@ -90,3 +92,75 @@ tasks.getByName("compileJava9Java", JavaCompile::class) {
     targetCompatibility = "1.9"
     sourceCompatibility = "1.9"
 }
+
+publishing {
+    repositories {
+        maven {
+            name = "Central"
+            afterEvaluate {
+                url = if (version.toString().endsWith("SNAPSHOT")) {
+                    // uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+                    uri("https://oss.sonatype.org/content/repositories/snapshots")
+                } else {
+                    // uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
+                    uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+                }
+            }
+
+            credentials {
+                username = properties["ossrhUsername"]?.toString() ?: System.getenv("OSSRH_USERNAME")
+                password = properties["ossrhPassword"]?.toString() ?: System.getenv("OSSRH_PASSWORD")
+            }
+        }
+    }
+    publications {
+        withType<MavenPublication> {
+            val pubName = name.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else "$it" }
+            afterEvaluate {
+                val emptyJavadocJar by tasks.register<org.gradle.api.tasks.bundling.Jar>("emptyJavadocJar$pubName") {
+                    archiveClassifier = "javadoc"
+                    archiveBaseName = artifactId
+                }
+                artifact(emptyJavadocJar)
+            }
+            pom {
+                name.set("kotlinx-serialization-bencoding")
+                description.set("A Kotlin serialization codec for bencoding format.")
+                url.set("https://github.com/iseki0/kotlinx-serialization-bencoding")
+                licenses {
+                    license {
+                        name.set("Apache-2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("iseki0")
+                        name.set("iseki zero")
+                        email.set("iseki@iseki.space")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/iseki0/kotlinx-serialization-bencoding.git")
+                    developerConnection.set("scm:git:https://github.com/iseki0/kotlinx-serialization-bencoding.git")
+                    url.set("https://github.com/iseki0/kotlinx-serialization-bencoding")
+                }
+            }
+        }
+    }
+}
+
+afterEvaluate {
+    signing {
+        // To use local gpg command, configure gpg options in ~/.gradle/gradle.properties
+        // reference: https://docs.gradle.org/current/userguide/signing_plugin.html#example_configure_the_gnupgsignatory
+        useGpgCmd()
+        publishing.publications.forEach { sign(it) }
+    }
+}
+
+tasks.withType<AbstractArchiveTask>().configureEach {
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
+}
+
